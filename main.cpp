594e7f48d391fd3113f4708bdf00d31d19771eaf
Austin Brown <austinmbrown22@gmail.com>
2019-11-07 12:00:32 -0600

    nut

---
+++ main.cpp
@@ -0,0 +1,35 @@
+
+
+struct node{
+    int data;
+    node* left;
+    node* right;
+    int height;
+};
+
+class AVL{
+    public:
+    void makeEmpty(node* t);
+    node* insert(int x, node* t);
+    node* singleRightRotate(node* &t);
+    node* singleLeftRotate(node* &t);
+    node* doubleLeftRotate(node* &t);
+    node* doubleRightRotate(node* &t);
+    node* findMin(node* t){
+        while(t->left != nullptr){
+            findMin(t->left);
+        }
+        return t->data;
+    };
+    node* findMax(node* t){
+        while(t->right != nullptr){
+            findMin(t->right);
+        }
+        return t->data;
+    };
+    node* remove(int x, node* t);
+    int height(node* t);
+    int  getBalance(node* t);
+    void inorder(node* t);
+    void display();
+};
