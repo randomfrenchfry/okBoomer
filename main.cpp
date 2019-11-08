7d663e25c7401892ffca226f5b7f8cf5c9820e6f
Daniel-Aguila <danielbaguila@gmail.com>
2019-11-08 03:10:49 -0600

    did some functions

--- main.cpp
+++ main.cpp
@@ -9,8 +9,28 @@ struct node{
 
 class AVL{
     public:
+    node* root;
+    AVL(){root==nullptr;}
     void makeEmpty(node* t);
-    node* insert(int x, node* t);
+    node* insert(int x, node* t){
+        if(t== nullptr){
+            node*p=new node;
+            p->data = x;
+            p->left = nullptr;
+            p->right = nullptr;
+            p->height=1;
+            return p;
+        }
+        if(x < t->data){ //if the new data is less than previous node data
+            t->left = insert(x,t->left);
+        }
+        else if(x > t->data){ //if the new data is more than previous node data
+            t->right = insert(x,t->right);
+        }
+        else
+            return t;
+        //Need to finish this
+    }
     node* singleRightRotate(node* &t);
     node* singleLeftRotate(node* &t);
     node* doubleLeftRotate(node* &t);
@@ -29,8 +49,16 @@ class AVL{
     };
     //comment comment
     node* remove(int x, node* t);
-    int height(node* t);
-    int  getBalance(node* t);
+    int height(node* t){
+        if(t== nullptr)
+            return 0;
+        return t->height;
+    }
+    int getBalance(node* t){
+        if(t== nullptr)
+            return 0;
+        return height(t->left)-height(t->right);
+    }
     void inorder(node* t);
     void display();
 };
